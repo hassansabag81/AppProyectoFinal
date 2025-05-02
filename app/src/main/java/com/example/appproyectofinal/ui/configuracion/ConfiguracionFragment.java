65ed02824c320551453ceda5beeec90afa51385c
XcodeDeveloper1Abacus <alu.20130486@correo.itlalaguna.edu.mx>
2025-05-02 17:44:41 -0600

    Pantalla Configuracion
    Le puse la pantalla coonnfiguracion y otras monadas

--- app/src/main/java/com/example/appproyectofinal/ui/configuracion/ConfiguracionFragment.java
+++ app/src/main/java/com/example/appproyectofinal/ui/configuracion/ConfiguracionFragment.java
@@ -1,12 +1,27 @@
 package com.example.appproyectofinal.ui.configuracion;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+
+import com.example.appproyectofinal.R;
 import com.example.appproyectofinal.databinding.FragmentConfiguracionBinding;
 
 public class ConfiguracionFragment extends Fragment {
@@ -23,6 +38,12 @@ public class ConfiguracionFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = FragmentConfiguracionBinding.inflate(inflater, container, false);
+        setHasOptionsMenu(true);
+
+        binding.contactsBtn.setOnClickListener(v -> {
+            DialogoNewContact();
+        });
+
         return binding.getRoot();
     }
 
@@ -31,4 +52,66 @@ public class ConfiguracionFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_save, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_guardar) {
+            // Aquí va tu lógica de guardado
+            Toast.makeText(getContext(), "Información guardada", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void cargarImagen(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent, "Seleccionar"),10);
+    }
+
+    public void DialogoNewContact() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View dialogView = inflater.inflate(R.layout.add_contact, null);
+        builder.setTitle("Agregar contacto");
+        builder.setView(dialogView);
+
+        EditText nombreContacto = dialogView.findViewById(R.id.nombreCE);
+        EditText telefonoContacto = dialogView.findViewById(R.id.telefonoCE);
+        RadioGroup parentescoContacto = dialogView.findViewById(R.id.radioGroup);
+
+        builder.setPositiveButton("Guardar Contacto", null);
+        builder.setNegativeButton("Cancelar", (dialog, which) -> dialog.dismiss());
+
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v -> {
+            if (validarCampos(nombreContacto, telefonoContacto, parentescoContacto)) {
+                contactoGuardado();
+                dialog.dismiss();
+            } else {
+                Toast.makeText(getContext(), "Error: Falta campos por rellenar", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    private boolean validarCampos(EditText nombre, EditText telefono, RadioGroup parentesco) {
+        if (nombre.getText().toString().trim().isEmpty() || telefono.getText().toString().trim().isEmpty() || parentesco.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(nombre.getContext(), "Error: falta campos por rellenar", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    public void contactoGuardado(){
+        Toast.makeText(getContext(), "Contacto guardado", Toast.LENGTH_SHORT).show();
+    }
+
 }
