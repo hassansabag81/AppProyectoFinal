64e2f3c3e6e3857998597428166219255554d323
Juan Ramiez <juanjoser2103@gmail.com>
2025-05-02 17:52:46 -0600

    Cambios estructura and historial

--- app/src/main/java/com/example/appproyectofinal/Estructura.java
+++ app/src/main/java/com/example/appproyectofinal/Estructura.java
@@ -2,12 +2,31 @@ package com.example.appproyectofinal;
 
 import android.provider.BaseColumns;
 
-public class Estructura {
-    public Estructura(){ }
+public final class Estructura {
 
-    public static abstract class EstructuraTabla implements BaseColumns{
-        public static final String TABLE_NAME = "tablaContactos";
-        public static final String COLUMN_NAME = "nombre";
-        public static final String COLUMN_PHONE = "telefono";
+    private Estructura() {}
+
+    public static class Usuario implements BaseColumns {
+        public static final String TABLE_NAME = "usuario";
+        public static final String COLUMN_NOMBRE = "nombre";
+        public static final String COLUMN_APELLIDO_P = "apellido_paterno";
+        public static final String COLUMN_APELLIDO_M = "apellido_materno";
+        public static final String COLUMN_TELEFONO = "telefono";
+        public static final String COLUMN_DIRECCION = "direccion";
+        public static final String COLUMN_IMAGEN = "imagen";
+    }
+
+    public static class Contacto implements BaseColumns {
+        public static final String TABLE_NAME = "contacto";
+        public static final String COLUMN_NOMBRE = "nombre";
+        public static final String COLUMN_TELEFONO = "telefono";
+    }
+
+    public static class HistorialLlamadas implements BaseColumns {
+        public static final String TABLE_NAME = "historial_llamadas";
+        public static final String COLUMN_CONTACTO_ID = "contacto_id";
+        public static final String COLUMN_FECHA = "fecha";
+        public static final String COLUMN_TIPO = "tipo"; // entrante, saliente, perdida
     }
 }
+
