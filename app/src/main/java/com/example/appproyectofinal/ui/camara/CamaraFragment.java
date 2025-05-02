12875ccfa9e6a4bff6d540b94e284386b067c713
Juan Ramiez <juanjoser2103@gmail.com>
2025-05-01 19:26:55 -0600

    Cambios Camara

--- app/src/main/java/com/example/appproyectofinal/ui/camara/CamaraFragment.java
+++ app/src/main/java/com/example/appproyectofinal/ui/camara/CamaraFragment.java
@@ -1,21 +1,24 @@
 package com.example.appproyectofinal.ui.camara;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import com.example.appproyectofinal.databinding.FragmentCamaraBinding;
 
 public class CamaraFragment extends Fragment {
 
     private FragmentCamaraBinding binding;
-
-    public CamaraFragment() {
-        // Constructor público vacío requerido
-    }
+    private ActivityResultLauncher<Intent> takePictureLauncher;
 
     @Nullable
     @Override
@@ -23,7 +26,34 @@ public class CamaraFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = FragmentCamaraBinding.inflate(inflater, container, false);
+
+
+        takePictureLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Bundle extras = result.getData().getExtras();
+                        Bitmap imageBitmap = (Bitmap) extras.get("data");
+                        binding.imgFoto.setImageBitmap(imageBitmap);
+                    }
+                }
+        );
+
+        binding.fotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tomarFoto();
+            }
+        });
+
         return binding.getRoot();
+    }
+
+    private void tomarFoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
+            takePictureLauncher.launch(takePictureIntent); // Usar el launcher en lugar de startActivityForResult
+        }
     }
 
     @Override
